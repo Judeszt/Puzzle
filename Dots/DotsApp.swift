aafeb5b0f6ef14542adfc761b57b667391a3c0fa
Judeszt <126808542+Judeszt@users.noreply.github.com>
2023-03-08 06:37:15 -0800

    Initial Commit

---
+++ Dots/DotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DotsApp.swift
+//  Dots
+//
+//  Created by Jude Sztraicher on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct DotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
